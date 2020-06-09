dacf82857edc42429a2cec1b4a001875a45cfa0d
Charlene Gipulan <charlenegipulan@gmail.com>
2020-06-09 00:17:39 -0700

    create global state and provider components

---
+++ src/context/AppReducer.js
@@ -0,0 +1,7 @@
+//how we specify application state changes in responsie to certain actions to our stoire/context
+export default (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
