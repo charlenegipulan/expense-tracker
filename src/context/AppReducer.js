f3958a32cfd4448be503980c85d265314d42e819
Charlene Gipulan <charlenegipulan@gmail.com>
2020-06-09 19:15:12 -0700

    add delete transaction functionality

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,11 @@
 //how we specify application state changes in responsie to certain actions to our stoire/context
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
         default:
             return state;
     }
