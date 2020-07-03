1469eddc0616fe809f12df95c1029759186a51de
Charlene Gipulan <charlenegipulan@gmail.com>
2020-07-02 23:38:42 -0700

    create get transaction function controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction');
+
 //GET ALL TRANSACTIONS
 //route GET /api/v1/transactions
 //access Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET TRANSACTIONS');
+exports.getTransactions = async (req, res, next) => {
+   try {
+        const transactions = await Transaction.find();
+
+        return res.status(200).json({
+            succes: true,
+            count: transactions.length,
+            data: transactions
+        });
+   } catch {
+        res.send(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+   }
 } 
 
 //Add TRANSACTIONS
 //route POST /api/v1/transactions
 //access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     res.send('POST TRANSACTIONS');
 } 
 
 //DELETE TRANSACTIONS
 //route DELETE /api/v1/transactions/:id
 //access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('Delete TRANSACTIONS');
 } 
