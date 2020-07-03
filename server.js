a0e2728250b1c73392bea7853d519f76c4107205
Charlene Gipulan <charlenegipulan@gmail.com>
2020-07-03 11:37:54 -0700

    call morgan

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 
 app.use(express.json()); //allows us to use body parser
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 app.use('/api/v1/transactions', transactions);
 
 
