0edd5f118df3c9d4d0e8e109552e5b166f29355e
Charlene Gipulan <charlenegipulan@gmail.com>
2020-07-02 23:29:22 -0700

    create transaction schema

---
+++ models/Transaction.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add some text']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Please add a positive or negative number']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    category: {
+        type: String,
+        required: [true, 'Please add category']
+    }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
